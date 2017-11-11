1542c0f4da0bc54650a8f4a2292687f22aba7b14
Matthieu Landès <matthieu.poubel37@gmail.com>
2017-11-11 19:35:20 +0100

    add logging tests+offical doc of Tornado

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This is a simple web service with tornado.
  * output
 
 ## Some help
+ * [Offical doc](http://www.tornadoweb.org/en/stable/guide/structure.html)
  * [Some examples](http://www.drdobbs.com/open-source/building-restful-apis-with-tornado/240160382)
  * [Return status](https://solidgeargroup.com/best-practices-rest-api)
 
--- server.py
+++ server.py
@@ -6,7 +6,8 @@ import tornado.web
 #import tornado.log
 import logging
 
-app_log = logging.getLogger("tornado.application")
+app_log = logging.getLogger(__name__)
+app_log.setLevel(logging.INFO)
 
 class VersionHandler(tornado.web.RequestHandler):
     def get(self):
@@ -24,12 +25,26 @@ class ErrorHandler(tornado.web.RequestHandler):
         else:
             raise tornado.web.HTTPError(500)
 
+class TestHandler(tornado.web.RequestHandler):
+    def initialize(self):
+        app_log.info('Hello Test : %s', self.request)
+
+    def get(self):
+        self.write("request test")
+        filters = self.request.arguments
+        for k,v in filters.items():
+            print "arg {0} : {1}".format(k, v)
+        self.set_status(200)
+
+
 application = tornado.web.Application([
     (r"/error/([0-9]+)", ErrorHandler),
+    (r"/test", TestHandler),
     (r"/version", VersionHandler)
 ])
 
 if __name__ == "__main__":
 
+    logging.getLogger("tornado.access").setLevel(logging.INFO)
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
